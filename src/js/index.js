73859719cf70e0dc60de11b18e3f9727bcaff863
paprajapati9 <paprajapati9@gmail.com>
2021-02-04 20:30:46 +0530

    added icons and scenerio handling

--- src/js/index.js
+++ src/js/index.js
@@ -1,26 +1,31 @@
 document.addEventListener("DOMContentLoaded", ()=>{
 
+    const dashboardContainer = document.querySelector(".pp-dashboard-container");
     const dashboard = document.querySelector(".pp-dashboard");
+    const scoreboard = document.querySelector(".pp-scoreboard");
     const size = 8;
     const blocks = [];
+    var score = 0;
 
-    const blockColors = [
-        'red',
-        'orange',
-        'yellow',
-        'purple',
-        'green',
-        'blue'
+    dashboardContainer.style.backgroundImage = 'url(https://miltontan.com/photography/wp-content/uploads/2013/01/portrait-bg.jpg)';
+
+    const blockIcons = [
+        'url(assets/icons/mango.png)',
+        'url(assets/icons/pineapple.png)',
+        'url(assets/icons/bell-pepper.png)',
+        'url(assets/icons/watermelon.png)',
+        'url(assets/icons/apple.png)',
+        'url(assets/icons/blueberry.png)'
     ];
     
     function createBoard() {
         for (let i = 0; i < size*size; i++) {
             let block = document.createElement('div'),
-                randomColor = Math.floor(Math.random() * blockColors.length);
-            block.style.background = blockColors[randomColor];
+                randomColor = Math.floor(Math.random() * blockIcons.length);
+            block.style.backgroundImage = blockIcons[randomColor];
             block.setAttribute('draggable', true);
             block.setAttribute('id', `${i}`);
-            block.className = 'pp-block';
+            block.className = 'pp-block pp-image-block';
             dashboard.appendChild(block);
             blocks.push(block);           
         }
@@ -45,7 +50,7 @@ document.addEventListener("DOMContentLoaded", ()=>{
 
     function attachDragStartEvent(block) {
         block.addEventListener("dragstart", function( e ) {
-            colorDragged = this.style.background;
+            colorDragged = this.style.backgroundImage;
             blockDragged = parseInt(this.id, 10);
             console.log(colorDragged, "color dragged");
             console.log(this.id, "dragstart");
@@ -65,11 +70,10 @@ document.addEventListener("DOMContentLoaded", ()=>{
             if (blockReplaced && validMove) { 
                 blockReplaced = null;
             } else if(blockReplaced && !validMove){
-                blocks[blockDragged].style.background = colorDragged;
-                blocks[blockReplaced].style.background = colorReplaced;
-            } else blocks[blockDragged].style.background = colorDragged;
+                blocks[blockDragged].style.backgroundImage = colorDragged;
+                blocks[blockReplaced].style.backgroundImage = colorReplaced;
+            } else blocks[blockDragged].style.backgroundImage = colorDragged;
             
-            checkMatchesForThree();
             console.log(this.id, "dragend");
         }, false);
     }
@@ -97,10 +101,10 @@ document.addEventListener("DOMContentLoaded", ()=>{
     function attachDropEvent(block) {
         block.addEventListener("drop", function( e ) {
             console.log(this.id, "drop");
-            colorReplaced = this.style.background;
+            colorReplaced = this.style.backgroundImage;
             console.log(blockDragged, "dragged");
-            this.style.background = colorDragged;
-            blocks[blockDragged].style.background = colorReplaced;
+            this.style.backgroundImage = colorDragged;
+            blocks[blockDragged].style.backgroundImage = colorReplaced;
             blockReplaced = parseInt(this.id, 10);
         }, false);
     }
@@ -109,32 +113,69 @@ document.addEventListener("DOMContentLoaded", ()=>{
         for (let i = 0; i < blocks.length; i++) {
             let matchRow = [i, i+1, i+2],
                 matchColumn = [i, i+size, i+2*size],
-                colorCheck = blocks[i].style.background,
+                colorCheck = blocks[i].style.backgroundImage,
                 skipRowCheck = 0,
                 skipColumnCheck = 0;
 
-            const isBlank = blocks[i].style.background === '';
+            const isBlank = blocks[i].style.backgroundImage === '';
             const invalid = [6, 7, 14, 15, 22, 23, 30, 31, 38, 39, 46, 47, 54, 55];
             if(invalid.includes(i)) skipRowCheck = 1;
             if(i > 46) skipColumnCheck = 1;
 
-            if(!skipRowCheck){
-                if(matchRow.every(index => blocks[index] && blocks[index].style.background === colorCheck && !isBlank)){
-                    matchRow.forEach(element => {
-                        blocks[element].style.background = '';
-                    });
-                } 
-            }  
+            checkAndRemoveMatch(matchRow, colorCheck, isBlank, skipRowCheck, 3);
             
-            if(!skipColumnCheck){
-                if(matchColumn.every(index => blocks[index] && blocks[index].style.background === colorCheck && !isBlank)){
-                    matchColumn.forEach(element => {
-                        blocks[element].style.background = '';
-                    });
-                }  
+            checkAndRemoveMatch(matchColumn, colorCheck, isBlank, skipColumnCheck, 3);
+        }
+    }
+
+    function checkMatchesForFour() {
+        for (let i = 0; i < blocks.length; i++) {
+            let matchRow = [i, i+1, i+2, i+3],
+                matchColumn = [i, i+size, i+2*size, i+3*size],
+                colorCheck = blocks[i].style.backgroundImage,
+                skipRowCheck = 0,
+                skipColumnCheck = 0;
+
+            const isBlank = blocks[i].style.backgroundImage === '';
+            const invalid = [5, 6, 7, 13, 14, 15, 21, 22, 23, 29, 30, 31, 37, 38, 39, 45, 46, 47, 53, 54, 55];
+            if(invalid.includes(i)) skipRowCheck = 1;
+            if(i > 46) skipColumnCheck = 1;
+
+            checkAndRemoveMatch(matchRow, colorCheck, isBlank, skipRowCheck, 4);
+            
+            checkAndRemoveMatch(matchColumn, colorCheck, isBlank, skipColumnCheck, 4);
+        }
+    }
+
+    function checkAndRemoveMatch(matchArray, colorCheck, isBlank, skip, scoreIncrease) {
+        if(skip) return;
+        if(matchArray.every(index => blocks[index] && blocks[index].style.backgroundImage === colorCheck && !isBlank)){
+            matchArray.forEach(element => {
+                blocks[element].style.backgroundImage = '';
+            });
+            score += scoreIncrease;
+            scoreboard.innerHTML = score;
+        }  
+    }
+
+    function moveDown() {
+        for (let i = 0; i < 55; i++) {
+            if( blocks[i+size].style.backgroundImage == '' ){
+                blocks[i+size].style.backgroundImage = blocks[i].style.backgroundImage;
+                blocks[i].style.backgroundImage = '';
+            }
+            const firstRow = [0, 1, 2, 3, 4, 5, 6, 7];
+            const isFirstRow = firstRow.includes(i);
+            if(isFirstRow && blocks[i].style.backgroundImage == ''){
+                randomColor = Math.floor(Math.random() * blockIcons.length);
+                blocks[i].style.backgroundImage = blockIcons[randomColor];
             }
         }
     }
 
-    checkMatchesForThree();
+    window.setInterval(function () {
+        moveDown();
+        checkMatchesForFour();
+        checkMatchesForThree();
+    }, 100)
 });
